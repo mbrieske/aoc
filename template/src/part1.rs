46027fe63a08dacad8ded83a327b2f7fd31f0ba3
Malte Brieske <mbrieske@users.noreply.github.com>
2023-12-13 17:12:36 +0100

    add rstest to template

--- template/src/part1.rs
+++ template/src/part1.rs
@@ -1,16 +1,26 @@
 pub fn solve(input: &str) -> usize {
-    let mut lines = input.lines();
+    input.lines().map(process_line).sum()
+}
+
+fn process_line(_line: &str) -> usize {
     todo!()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
 
     #[test]
     fn example() {
         let example = "";
-
         assert_eq!(solve(example), 0);
+    }
+
+    #[rstest]
+    #[case("asd", 2)]
+    #[case("fgh", 2)]
+    fn test_process_line(#[case] input: &str, #[case] expected: usize) {
+        assert_eq!(process_line(input), expected);
     }
 }
