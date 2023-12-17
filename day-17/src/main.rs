57825c17fdd94b1b180ecd4b83798051f891fdaf
Malte Brieske <mbrieske@users.noreply.github.com>
2023-12-17 15:06:53 +0100

    day 17 part 1

---
+++ day-17/src/main.rs
@@ -0,0 +1,9 @@
+use day_17::part1::solve;
+// use day_xx::part2::solve;
+use std::fs::read_to_string;
+
+fn main() {
+    let input = read_to_string("res/input").unwrap();
+
+    println!("{}", solve(&input));
+}
