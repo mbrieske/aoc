42af959a12350c3ff11c01075588ebd17a1b70c9
Malte Brieske <mbrieske@users.noreply.github.com>
2023-12-18 14:50:51 +0100

    day 18 cleanup part1

--- day-18/src/part1.rs
+++ day-18/src/part1.rs
@@ -1,17 +1,20 @@
 use grid::Grid;
-use std::fmt::Display;
+use std::{
+    cmp::{max, min},
+    fmt::Display,
+};
 
 #[allow(dead_code)]
 #[derive(Clone)]
 enum Terrain {
-    Trench { color: Vec<u8> },
+    Trench,
     Ground,
 }
 
 impl Display for Terrain {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Terrain::Trench { color: _ } => write!(f, "#"),
+            Terrain::Trench => write!(f, "#"),
             Terrain::Ground => write!(f, "."),
         }
     }
@@ -49,35 +52,65 @@ impl Default for Terrain {
 }
 
 pub fn solve(input: &str) -> usize {
-    let mut grid: Grid<Terrain> = Grid::from_vec(vec![Terrain::Trench { color: vec![0; 3] }; 1], 1);
-    let mut position = Position { x: 0, y: 0 };
+    let directions: Vec<Direction> = input
+        .lines()
+        .map(|line| {
+            let mut parts = line.split_whitespace();
+            let (dir_char, len) = (
+                parts.next().unwrap(),
+                parts.next().unwrap().parse().unwrap(),
+            );
+            Direction::from((dir_char, len))
+        })
+        .collect();
 
-    input.lines().for_each(|line| {
-        let mut parts = line.split_whitespace();
-        let (dir_char, len, color) = (
-            parts.next().unwrap(),
-            parts.next().unwrap().parse().unwrap(),
-            hex::decode(&parts.next().unwrap()[2..8]).unwrap(),
-        );
-        let dir = Direction::from((dir_char, len));
-        extend_grid(&mut grid, &mut position, dir, color)
-    });
+    let (_, x_min, x_max, y_min, y_max) = directions.iter().fold(
+        (Position { x: 0, y: 0 }, 0, 0, 0, 0),
+        |(mut pos, x_min, x_max, y_min, y_max), direction| {
+            match direction {
+                Direction::Left(dist) => pos.x -= dist,
+                Direction::Right(dist) => pos.x += dist,
+                Direction::Up(dist) => pos.y -= dist,
+                Direction::Down(dist) => pos.y += dist,
+            };
+            (
+                pos,
+                min(pos.x, x_min),
+                max(pos.x, x_max),
+                min(pos.y, y_min),
+                max(pos.y, y_max),
+            )
+        },
+    );
+
+    let mut grid: Grid<Terrain> = Grid::from_vec(
+        vec![Terrain::Ground; ((x_max + 1 - x_min) * (y_max + 1 - y_min)) as usize],
+        (x_max + 1 - x_min) as usize,
+    );
+
+    let mut position = Position {
+        x: -x_min,
+        y: -y_min,
+    };
+    directions
+        .into_iter()
+        .for_each(|dir| mark_perimeter(&mut grid, &mut position, dir));
 
     let start_pos = find_inside(&grid);
     flood(&mut grid, start_pos);
     grid.iter()
-        .filter(|&t| matches!(t, Terrain::Trench { color: _ }))
+        .filter(|&t| matches!(t, Terrain::Trench))
         .count()
 }
 
 fn find_inside(grid: &Grid<Terrain>) -> Position {
     for y in 0..grid.rows() {
         let mut is_perimeter = false;
-        'x_loop: for (x, t) in grid.iter_row(y).enumerate() {
+        for (x, t) in grid.iter_row(y).enumerate() {
             match (is_perimeter, t) {
-                (false, Terrain::Trench { color: _ }) => is_perimeter = true,
+                (false, Terrain::Trench) => is_perimeter = true,
                 (false, Terrain::Ground) => (),
-                (true, Terrain::Trench { color: _ }) => break 'x_loop,
+                (true, Terrain::Trench) => break,
                 (true, Terrain::Ground) => {
                     return Position {
                         x: x as isize,
@@ -90,51 +123,29 @@ fn find_inside(grid: &Grid<Terrain>) -> Position {
     unreachable!()
 }
 
-fn extend_grid(grid: &mut Grid<Terrain>, pos: &mut Position, dir: Direction, color: Vec<u8>) {
+fn mark_perimeter(grid: &mut Grid<Terrain>, pos: &mut Position, dir: Direction) {
     match dir {
         Direction::Up(len) => {
+            for i in 1..=len {
+                *grid.get_mut((pos.y - i) as usize, pos.x as usize).unwrap() = Terrain::Trench;
+            }
             pos.y -= len;
-            while pos.y < 0 {
-                grid.insert_row(0, vec![Terrain::default(); grid.cols()]);
-                pos.y += 1;
-            }
-            for i in 0..len {
-                *grid.get_mut((pos.y + i) as usize, pos.x as usize).unwrap() = Terrain::Trench {
-                    color: color.clone(),
-                };
-            }
         }
         Direction::Down(len) => {
-            while (pos.y + len) as usize >= grid.rows() {
-                grid.push_row(vec![Terrain::default(); grid.cols()]);
-            }
             for i in 1..=len {
-                *grid.get_mut((pos.y + i) as usize, pos.x as usize).unwrap() = Terrain::Trench {
-                    color: color.clone(),
-                };
+                *grid.get_mut((pos.y + i) as usize, pos.x as usize).unwrap() = Terrain::Trench;
             }
             pos.y += len;
         }
         Direction::Left(len) => {
+            for i in 1..=len {
+                *grid.get_mut(pos.y as usize, (pos.x - i) as usize).unwrap() = Terrain::Trench;
+            }
             pos.x -= len;
-            while pos.x < 0 {
-                grid.insert_col(0, vec![Terrain::default(); grid.rows()]);
-                pos.x += 1;
-            }
-            for i in 0..len {
-                *grid.get_mut(pos.y as usize, (pos.x + i) as usize).unwrap() = Terrain::Trench {
-                    color: color.clone(),
-                };
-            }
         }
         Direction::Right(len) => {
-            while (pos.x + len) as usize >= grid.cols() {
-                grid.push_col(vec![Terrain::default(); grid.rows()]);
-            }
             for i in 1..=len {
-                *grid.get_mut(pos.y as usize, (pos.x + i) as usize).unwrap() = Terrain::Trench {
-                    color: color.clone(),
-                };
+                *grid.get_mut(pos.y as usize, (pos.x + i) as usize).unwrap() = Terrain::Trench;
             }
             pos.x += len;
         }
@@ -149,7 +160,7 @@ fn flood(grid: &mut Grid<Terrain>, start_pos: Position) {
         let (col, row) = (pos.x as usize, pos.y as usize);
 
         if let Some(Terrain::Ground) = grid.get(row, col) {
-            *grid.get_mut(row, col).unwrap() = Terrain::Trench { color: vec![0; 3] };
+            *grid.get_mut(row, col).unwrap() = Terrain::Trench;
             for &(dx, dy) in &[(-1, 0), (1, 0), (0, -1), (0, 1)] {
                 let new_pos = Position {
                     x: pos.x + dx,
@@ -173,12 +184,10 @@ fn flood(grid: &mut Grid<Terrain>, start_pos: Position) {
 #[cfg(test)]
 mod tests {
     use super::*;
-    // use rstest::rstest;
 
     #[test]
     fn example() {
-        let example = "R 6 (#70c710)
-D 5 (#0dc571)
+        let example = "D 5 (#0dc571)
 L 2 (#5713f0)
 D 2 (#d2c081)
 R 2 (#59c680)
@@ -190,14 +199,8 @@ U 2 (#caa171)
 R 2 (#7807d2)
 U 3 (#a77fa3)
 L 2 (#015232)
-U 2 (#7a21e3)";
+U 2 (#7a21e3)
+R 6 (#70c710)";
         assert_eq!(solve(example), 62);
     }
-
-    // #[rstest]
-    // #[case("asd", 2)]
-    // #[case("fgh", 2)]
-    // fn test_process_line(#[case] input: &str, #[case] expected: isize) {
-    //     assert_eq!(process_line(input), expected);
-    // }
 }
